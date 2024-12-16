983f08d7e0be808171e1736f940c6a0998aa5746
iamgoodbytes <joris@goodbytes.be>
2024-12-16 12:09:01 +0100

    feat: add fundamentals

---
+++ topics/fundamentals.starter.js
@@ -0,0 +1,30 @@
+/*
+ ____  _  _  __ _  ____   __   _  _  ____  __ _  ____  __   __    ____ 
+(  __)/ )( \(  ( \(    \ / _\ ( \/ )(  __)(  ( \(_  _)/ _\ (  )  / ___)
+ ) _) ) \/ (/    / ) D (/    \/ \/ \ ) _) /    /  )( /    \/ (_/\\___ \
+(__)  \____/\_)__)(____/\_/\_/\_)(_/(____)\_)__) (__)\_/\_/\____/(____/
+ */
+
+// 1. link this script file to your page and open your developer console
+
+// 2. create a variable that keeps track of your name (can be let, const or var)
+
+// 3. create a variable that asks for your birthyear (use prompt() for that)
+// hint: prompt() returns a string, so you'll need to convert it to a number with parseInt()
+
+// 4. calculate your age (this will require grabbing the current year somehow)
+// hint: you can get the current year with new Date().getFullYear()
+
+// 5. log your age to the console
+
+// 6. write a function named greet() that wishes you a nice day like `Hi there, [your name]! Have a nice day!`
+// hint: you can use string interpolation with backticks to insert your name into the string
+// use the function to print the message to the console
+
+// 7. write a function named calculateAge() that calculates your age and logs it to the console
+// use the function to print your age to the console
+
+// 8. use prompt() to ask if you how you are feeling today ("good", "bad", "ok")
+// store the result in a variable
+// print an emoji to the console based on the result (😊, 😞, 😐)
+// kudos to you if you use a switch statement for this
