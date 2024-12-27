57e735be4d1887db0c80b31b7f728d4b2bf8a98e
iamgoodbytes <joris@goodbytes.be>
2024-12-27 16:29:26 +0100

    feat: timer exercices

---
+++ topics/timers.starter.js
@@ -0,0 +1,15 @@
+/*
+  __  .__                             
+_/  |_|__| _____   ___________  ______
+\   __\  |/     \_/ __ \_  __ \/  ___/
+ |  | |  |  Y Y  \  ___/|  | \/\___ \ 
+ |__| |__|__|_|  /\___  >__|  /____  >
+               \/     \/           \/ 
+*/
+
+// 1 — start a timer and show a ⏰ in #clock after 10 seconds
+
+// 2 — click the start button to show a ticking clock in #timer that goes up every second
+// clicking stop should clear and stop the timer and reset the clock to 0
+
+// 3 —
