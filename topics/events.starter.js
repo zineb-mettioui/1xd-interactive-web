8481a0289f51e9c9103dc8ed7d9572d20fb78b17
iamgoodbytes <joris@goodbytes.be>
2024-12-27 16:03:48 +0100

    feat: events exercices

--- topics/events.starter.js
+++ topics/events.starter.js
@@ -7,3 +7,17 @@
 \____/ \_/ \___|_| |_|\__|___/
                                                            
 */
+
+// 1 — Click on the button and show an alert()
+// hint: first select the button, then add an event listener to it
+
+// 2 — Hover over the square (#zone1) and change its background color
+
+// 3 — Click on the link to display the hidden Message
+// hint: first see how the message is hidden, then do the opposite
+// hint2: prevent the default behavior of the link by using .preventDefault()
+
+// 4 — Similar to question number 2, this time use event bubbling to change the background color of the squares in #bubbleZone
+// hint: with event bubbling you can select the parent element and listen for events on its children to avoid adding multiple event listeners
+
+// 5 — Bonus: use mouseout to change the background color back to the original one on the squares in question 2 or 4
