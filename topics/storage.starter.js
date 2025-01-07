90f07747989336140a0f53db1471e1698384d8cf
iamgoodbytes <joris@goodbytes.be>
2025-01-07 11:38:58 +0100

    feat: add storage exercices

---
+++ topics/storage.starter.js
@@ -0,0 +1,20 @@
+/*
+          __                                      
+  _______/  |_  ________________     ____   ____  
+ /  ___/\   __\/  _ \_  __ \__  \   / ___\_/ __ \ 
+ \___ \  |  | (  <_> )  | \// __ \_/ /_/  >  ___/ 
+/____  > |__|  \____/|__|  (____  /\___  / \___  >
+     \/                         \//_____/      \/ 
+*/
+
+// 1 - click on the button, then add your name to localstorage in the key "myName"
+
+// 2 - click on the button to read the value of "myName" from localstorage and display it span#myName
+
+// 3 - click on remove button to remove "myName" from localstorage
+
+// 4 - click on the button to add a movie to the localstorage in the key "movies", show movies in the ul#movieList
+// hint: the value of movies should be an array of strings
+// hint: you can use JSON.stringify to convert an array to string
+// hint: you can use JSON.parse to convert a string to array
+// hint: you can use appendChild to add a new li to the ul#movieList
